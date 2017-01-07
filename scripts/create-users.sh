b4470f40e7d11658ceb4d1298e60cc5b4fa79ded
Joe Cheng <joe@rstudio.com>
2017-01-07 14:15:53 -0800

    Script fixes

--- scripts/create-users.sh
+++ scripts/create-users.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -e
+
+cd $(dirname $0)
+
+if [ -f ../passwords.txt ]; then
+  echo "Error: passwords.txt already exists" >&2
+  exit 1
+fi
+
 for i in {1000..1999}; do
   adduser --disabled-password --gecos "" guest$i
   PASSWORD=`pwgen`
-  echo "guest$i,$PASSWORD" >> passwords.txt
+  echo "guest$i,$PASSWORD" >> ../passwords.txt
   echo "guest$i:$PASSWORD" | chpasswd
   mkdir -p /home/guest$i/.rstudio/projects_settings
-  echo -n "/home/guest$i/user2016-tutorial-shiny/user2016-tutorial-shiny.Rproj" > /home/guest$i/.rstudio/projects_settings/switch-to-project
+  #echo -n "/home/guest$i/user2016-tutorial-shiny/user2016-tutorial-shiny.Rproj" > /home/guest$i/.rstudio/projects_settings/switch-to-project
 done
